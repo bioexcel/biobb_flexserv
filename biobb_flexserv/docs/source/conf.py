49d117ea0b341762879181df26987b2c0478309f
gbayarri <genis.bayarri@irbbarcelona.org>
2025-01-08 16:27:16 +0100

    Year updated

--- biobb_flexserv/docs/source/conf.py
+++ biobb_flexserv/docs/source/conf.py
@@ -71,7 +71,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'biobb_flexserv'
-copyright = u'2024, Bioexcel Project'
+copyright = u'2025, Bioexcel Project'
 author = u'Bioexcel Project'
 
 # The version info for the project you're documenting, acts as replacement for
